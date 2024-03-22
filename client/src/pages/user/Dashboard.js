57bdbd90172db1601fccf7220d34c36f0b0fb6d6
Aniketikhar <aniketikhar88@gmail.com>
2024-03-22 13:43:33 +0530

    creating private route for dashboard page

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+const Dashboard = () => {
+  return (
+    <Layout title={"Dashboard - Ecommerce App"}>
+      <h1>Dashborad Page - this is dashboard</h1>
+    </Layout>
+  );
+};
+
+export default Dashboard;
