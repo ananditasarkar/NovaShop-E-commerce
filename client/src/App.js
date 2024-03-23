573a6e61e4bf08ebc5d63ad881f62237af736eea
Aniketikhar <aniketikhar88@gmail.com>
2024-03-23 18:05:48 +0530

    Initializing user Dashboar and user Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,11 @@ import PrivateRoute from "./components/Routes/Private";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import AdminRoute from "./components/Routes/AdminRoute";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
+import CreateCategory from "./pages/Admin/CreateCategory";
+import CreateProducts from "./pages/Admin/CreateProducts";
+import User from "./pages/Admin/User";
+import Orders from "./pages/user/Orders";
+import Profile from "./pages/user/Profile";
 
 function App() {
 
@@ -25,9 +30,14 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
           <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProducts />} />
+          <Route path="admin/users" element={<User />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
--- client/src/pages/Admin/CreateProducts.js
+++ client/src/pages/Admin/CreateProducts.js
@@ -3,8 +3,7 @@ import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
 import { useAuth } from '../../context/auth'
 
-const AdminDashboard = () => {
-  const [auth] = useAuth();
+const CreateProducts = () => {
   return (
     <Layout>
         <div className="container-fluid m-3 p-3">
@@ -14,9 +13,7 @@ const AdminDashboard = () => {
             </div>
             <div className="col-md-9">
               <div className="card w-75 p-3">
-                <h3>Admin Name : {auth?.user?.name}</h3>
-                <h3>Admin Email : {auth?.user?.email}</h3>
-                 <h3>Admin Phone  : {auth?.user?.phone}</h3>
+               products
               </div>
             </div>
           </div>
@@ -24,6 +21,6 @@ const AdminDashboard = () => {
 
     </Layout>
   )
-  }
+}
 
-export default AdminDashboard
+export default CreateProducts
